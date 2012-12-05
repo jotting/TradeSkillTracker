7e48f0441502da06573648a64b44107a577c4771
Jace Otting <jace.otting@garmin.com>
2012-12-05 15:10:59 -0600

    A lot of functionality, some bad hand testing

--- tradeSkillTracker.lua
+++ tradeSkillTracker.lua
@@ -1,3 +1,98 @@
+function main()
+	EventFrame.skillPath = processFile("tailoring")
+	startConsole()
+end
+
+EventFrame = {skillPath=nil}
+Player = {skill=nil, skillLevel=1, items={}}
+
+function EventFrame:updateSkillPath()
+	local index = self.skillPath.current
+	local entry = self.skillPath[index]
+
+--	local entry = {
+--		requiredSkill = nil,
+--		number = nil,
+--		reagent = nil,
+--		skillLevel = nil,
+--		craftable = nil
+--	}
+	local gotoNext = false
+	if entry.requiredSkill ~= nil and entry.requiredSkill == Player.skill then
+		gotoNext = true
+	elseif entry.reagent ~= nil and Player.items[entry.reagent] ~= nil
+		and entry.number <= Player.items[entry.reagent].count then
+		gotoNext = true
+	elseif entry.craftable ~= nil and entry.skillLevel <= Player.skillLevel then
+		gotoNext = true
+	end
+
+	if gotoNext then
+		self.skillPath.current = self.skillPath.current + 1
+		EventFrame:updateSkillPath()
+	else
+		EventFrame:updateWindow()
+	end
+end
+
+function EventFrame:updateWindow()
+	print("-----")
+	local index = self.skillPath.current
+	local entry = self.skillPath[index]
+	for k,v in pairs(entry) do print(k, v) end
+end
+
+function EventFrame:onTrained(skillName)
+	if self.skillPath == nil then return end
+
+	local i = self.skillPath.current
+	local requiredSkill = self.skillPath[i].requiredSkill
+	if requiredSkill ~= nil and requiredSkill == skillName then
+		EventFrame:updateSkillPath()
+	end
+end
+
+function EventFrame:onSkillUp(skill)
+	if self.skillPath == nil or self.skillPath.skill ~= Player.skill then return end
+
+	local i = self.skillPath.current
+	local skillLevel = self.skillPath[i].skillLevel
+	if skillLevel ~= nil and skillLevel <= Player.skillLevel then
+		EventFrame:updateSkillPath()
+	end
+end
+
+function EventFrame:onAcquired(acquiredItem)
+	if self.skillPath == nil then return end
+
+	local i = self.skillPath.current
+	local entry = self.skillPath[i]
+	if entry.reagent ~= nil and entry.reagent == acquiredItem
+		and entry.number ~= nil and Player.items[acquiredItem] ~= nil then
+		local itemCount = Player.items[acquiredItem].count
+		if entry.number <= itemCount then
+			EventFrame:updateSkillPath()
+		else
+			print(entry.number.." / "..itemCount.." - "..entry.reagent)
+		end
+	end
+end
+
+function processFile(filename)
+	local skillFile = assert(io.open(filename, "r"), "Unable to open file")
+	local entries = {current = 1, skill=filename}
+	-- Process the file
+	for line in skillFile:lines() do
+		local entry = processLine(line)
+		if entry ~= nil then
+			table.insert(entries, entry)
+		end
+	end
+	skillFile:close()
+
+	return entries
+end
+
 function processLine(line)
 	local entry = {
 		requiredSkill = nil,
@@ -24,39 +119,46 @@ function processLine(line)
 	end
 
 	-- Craftable
-	local i,j,number,items = string.find(line, "^%(?(%d+)%)? (.*)")
+	local i,j,number,items = string.find(line, "^(%d+) (.*)")
 	if i ~= nil then
-		local isCount = string.find(line, "^%(%d+%)")
-		if isCount ~= nil then
-			entry.number = tonumber(number)
-		else
-			entry.skillLevel = number
-		end
-
+		entry.skillLevel = number
 		entry.craftable = items
 
 		return entry
 	end
 end
 
-function processFile(filename)
-	local skillFile = assert(io.open(filename, "r"), "Unable to open file")
-	local entries = {}
-	-- Process the file
-	for line in skillFile:lines() do
-		local entry = processLine(line)
-		if entry ~= nil then
-			table.insert(entries, entry)
+function startConsole()
+	EventFrame:updateSkillPath()
+	io.flush()
+	local input = ""
+	repeat
+		local i,j,command,argument,argument2 = string.find(input, "^(%a*) ?(%w*) ?(%w*)")
+		if i == nil then
+			input = ""
 		end
-	end
-	skillFile:close()
 
-	return lines
+		if string.lower(command) == "train" then
+			trained(nil)
+		elseif string.lower(input) == "skill" then
+			skillUp(argument)
+		elseif string.lower(input) == "item" then
+			if argument2 == nil then argument2 = 1 end
+			itemAcquired(argument, argument2)
+		elseif string.lower(input) == "fish" then
+			fishingUp()
+		end
+
+		io.write("> ")
+		io.flush()
+		input = io.read()
+	until isStop(input)
 end
 
 function isStop(input)
 	input = string.lower(input)
 	if input == "end" or
+		input == "exit" or
 		input == "stop" then
 		return true
 	end
@@ -65,51 +167,40 @@ function isStop(input)
 end
 
 function trained(thisSkill)
-	print("trained: "..tostring(thisSkill))
-end
-
-function itemCreated(currentItem)
-	print("item created: "..tostring(currentItem))
-end
-
-function skillUp(thisSkill, amount)
-	print("skill up: "..tostring(thisSkill).." +"..tostring(amount))
-end
-
-function itemAcquired(reagent1, reagent2)
-	print("item acquired: "..tostring(reagent1).." | "..tostring(reagent2))
-end
-
-function startConsole()
-	io.flush()
-	local input = io.read()
-	while not isStop(input) do
-		if string.lower(input) == "trained" then
-			trained(true)
-		elseif string.lower(input) == "trained bad" then
-			trained(false)
-		elseif string.lower(input) == "item" then
-			itemCreated(true)
-		elseif string.lower(input) == "item bad" then
-			itemCreated(false)
-		elseif string.lower(input) == "skill" then
-			skillUp(true, 1)
-		elseif string.lower(input) == "skill 3" then
-			skillUp(true, 3)
-		elseif string.lower(input) == "skill bad" then
-			skillUp(false, 1)
-		elseif string.lower(input) == "reagent" then
-			itemAcquired(true, false)
-		elseif string.lower(input) == "reagent second" then
-			itemAcquired(false, true)
-		elseif string.lower(input) == "reagent bad" then
-			itemAcquired(false, false)
-		end
-
-		io.flush()
-		input = io.read()
+	if thisSkill == nil then 
+		thisSkill = EventFrame.skillPath[EventFrame.skillPath.current].requiredSkill
 	end
+	if thisSkill == nil then return end
+	Player.skill = thisSkill
+	print("trained: "..thisSkill)
+
+	EventFrame:onTrained(thisSkill)
 end
 
-skillPath = processFile("tailoring")
-startConsole()
+function skillUp(amount)
+	if amount == nil then return end
+	Player.skillLevel = Player.skillLevel + amount
+	print(Player.skill.." +"..tostring(amount))
+
+	EventFrame:onSkillUp(Player.skill)
+end
+
+function fishingUp()
+	print("Fishing +"..tostring(amount))
+
+	EventFrame:onSkillUp("Fishing")
+end
+
+function itemAcquired(item, amount)
+	if item == nil then return end
+	if Player.items[item] == nil then
+		Player.items[item] = {count = 0}
+	end
+	Player.items[item].count = Player.items[item].count + amount
+	print(amount.." "..item.."(s) acquired.")
+
+	EventFrame:onAcquired(item)
+end
+
+
+main()
